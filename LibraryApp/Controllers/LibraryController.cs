5ae1ec7ed9c1a716ba0cc75903d766bccbe61320
Murad Jafarov <muradkk@code.edu.az>
2022-12-04 03:07:05 +0400

    added file

---
+++ LibraryApp/Controllers/LibraryController.cs
@@ -0,0 +1,100 @@
+﻿using Domain.Entities;
+using Service.Helpers;
+using Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.Controllers
+{
+    public class LibraryController
+    {
+        private readonly LibraryService _libraryService;
+        public LibraryController()
+        {
+            _libraryService= new LibraryService();
+        }
+
+
+        public void Create()
+        {
+            try
+            {
+                ConsoleColor.Yellow.WriteConsole("Add library name:");
+
+                string name = Console.ReadLine();
+
+                ConsoleColor.Yellow.WriteConsole("Add library seat count:");
+
+            SeatCount: string seatCountStr = Console.ReadLine();
+
+                int seatCount;
+
+                bool isParseSeatCount = int.TryParse(seatCountStr, out seatCount);
+
+                if (isParseSeatCount)
+                {
+                    Library library = new()
+                    {
+                        Name = name,
+                        SeatCount = seatCount
+                    };
+
+                    var result = _libraryService.Create(library);
+
+                    ConsoleColor.Green.WriteConsole($"id: {result.Id}, Name: {result.Name}, Seat count: {result.SeatCount}");
+
+
+                }
+                else
+                {
+                    ConsoleColor.Red.WriteConsole("Please add correct seat count:");
+                    goto SeatCount;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            
+
+
+
+        }
+
+        public void GetById() 
+        {
+            try
+            {
+                ConsoleColor.DarkMagenta.WriteConsole("Write id:");
+
+                id: string idStr = Console.ReadLine();
+
+                int id;
+
+                bool isParseId = int.TryParse(idStr, out id);
+                if (isParseId) 
+                {
+                    var result = _libraryService.GetById(id);
+                    ConsoleColor.Green.WriteConsole($"id: {result.Id}, Name: {result.Name}, Seat count: {result.SeatCount}");
+                }
+                else
+                {
+                    ConsoleColor.Red.WriteConsole("Please add correct id:");
+                    goto id;
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                ConsoleColor.Red.WriteConsole(ex.Message);
+            }
+        }
+    }
+}
---
+++ LibraryApp/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+using LibraryApp.Controllers;
+using Service.Helpers;
+using System.Runtime.InteropServices;
+
+LibraryController libraryController = new();
+
+
+while (true)
+{
+    ConsoleColor.Blue.WriteConsole("Select one option");
+    ConsoleColor.Blue.WriteConsole("Library options: 1 - Create, 2 - Get by id, 3 - Delete");
+
+    SelectOption:  string option = Console.ReadLine();
+
+    int selectOption;
+
+    bool isParseOption = int.TryParse(option, out selectOption);
+
+    if (isParseOption)
+    {
+        switch (selectOption)
+        {
+            case 1:
+                libraryController.Create();
+                break;
+            case 2:
+                libraryController.GetById();
+                break;
+            case 3:
+                Console.WriteLine("Delete");
+                break;
+            default:
+                Console.WriteLine("Select again true option");
+                goto SelectOption;
+
+        }
+    }
+    else
+    {
+        ConsoleColor.Red.WriteConsole("Select again true option");
+        goto SelectOption;
+    }
+
+
+
+
+
+}
